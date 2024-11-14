37bc4ceecf2d17b5eab295b6e6052aee24d3fe92
Hasan <hasan.dev04@gmail.com>
2024-11-14 22:40:15 +0600

    requirements text added

--- build_files.sh
+++ build_files.sh
@@ -4,8 +4,3 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --noinput
 
-if ! command -v pip &> /dev/null; then
-    echo "Python and pip are not installed. Installing now..."
-    apt-get update
-    apt-get install -y python3 python3-pip
-fi
