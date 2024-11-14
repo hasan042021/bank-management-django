caac43db3f120e4f1cad7979bc8463a57d0a9f1a
Hasan <hasan.dev04@gmail.com>
2024-11-14 23:13:21 +0600

    some issue solved

--- bank_managemnent_django/asgi.py
+++ bank_managemnent_django/asgi.py
@@ -14,4 +14,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bank_managemnent_django.settings")
 
 application = get_asgi_application()
-app = application
