9a82ba7f9e411cd91277040fb16a47bd14de7629
Hasan <hasan.dev04@gmail.com>
2024-11-14 23:26:25 +0600

    wsgi updated

--- bank_managemnent_django/wsgi.py
+++ bank_managemnent_django/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bank_managemnent_django.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bank_managemnent_django.settings")
 
 application = get_wsgi_application()
+
+app = application
