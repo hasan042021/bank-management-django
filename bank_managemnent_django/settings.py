ae124a6dde790d36f22ea67a62495955c53ba416
Hasan <hasan.dev04@gmail.com>
2024-11-14 22:23:38 +0600

    env added

--- bank_managemnent_django/settings.py
+++ bank_managemnent_django/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tuni!f%gcy*n=g1q_9x!ml+1ef%z7_dzk9+%mojbgza1nhau7-"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
