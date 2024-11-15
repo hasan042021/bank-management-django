dde69a8281ebe06669d7a27d049d617694145d90
Hasan <hasan.dev04@gmail.com>
2024-11-16 00:40:04 +0600

    static url updated

--- bank_managemnent_django/settings.py
+++ bank_managemnent_django/settings.py
@@ -142,9 +142,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Static files in your project
+STATIC_ROOT = os.path.join(
+    BASE_DIR, "staticfiles"
+)  # Static files collected by collectstatic
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
