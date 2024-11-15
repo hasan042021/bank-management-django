41005198ff9defec29d6ec99ead676a8ca771e95
Hasan <hasan.dev04@gmail.com>
2024-11-16 01:36:30 +0600

    files updated

--- bank_managemnent_django/settings.py
+++ bank_managemnent_django/settings.py
@@ -143,9 +143,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Static files in your project
-STATIC_ROOT = os.path.join(
-    BASE_DIR, "staticfiles"
-)  # Static files collected by collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
