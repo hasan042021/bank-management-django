bf9de952fe8a47d03c896ffa72d890f45c9c12e8
Hasan <hasan.dev04@gmail.com>
2024-11-15 16:53:49 +0600

    statics added

--- bank_managemnent_django/settings.py
+++ bank_managemnent_django/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
